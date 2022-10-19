a8fabf197558bba7a6f7e0a135002157067c12ec
xiaoquanquanxin <xxkl123@126.com>
2022-10-19 23:16:50 +0800

    index

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,31 +1,45 @@
 import { Canvas, View } from "@tarojs/components";
 import Taro from "@tarojs/taro";
 import { useEffect, useState } from "react";
-import { calcCanvas, loadImage, renderText } from "../../utils/core";
+import { calcCanvas, loadImage } from "../../utils/core";
 
 import "./index.scss";
+import { renderText } from "../../utils/fctx";
 
 const IndexPage = () => {
-  const [imgInfo, setImgInfo] = useState();
+  // const [imgInfo, setImgInfo] = useState();
+
+  const [textChartSize, setTextChartSize] = useState({
+    width: 100,
+    height: 100
+  });
   useEffect(() => {}, []);
   useEffect(() => {
     console.clear();
     Taro.nextTick(() => {
       wx.createSelectorQuery()
-        .select("#chart-temp")
+        .select("#main-chart")
         .fields({ node: true, size: true })
         .exec(async res => {
           const canvas = res[0].node;
           const ctx = calcCanvas(canvas);
           await loadImage(canvas, ctx, canvas.width, canvas.height);
-          renderText(canvas, ctx, "一二三四五十");
+        });
+      wx.createSelectorQuery()
+        .select("#text-chart")
+        .fields({ node: true, size: true })
+        .exec(async res => {
+          const canvas = res[0].node;
+          renderText(canvas, "一二三四五sss", setTextChartSize);
         });
     });
   }, []);
   return (
     <View>
       {/*<Canvas type="2d" className={"chart"} id={"chart"}/>*/}
-      <Canvas type="2d" className={"chart"} id={"chart-temp"} />
+      <Canvas type="2d" className={"chart"} id="main-chart" />
+      <Canvas type="2d" style={textChartSize} id="text-chart" />
+      <View>在这里⬆️⬆️⬆️⬆️</View>
     </View>
   );
 };
